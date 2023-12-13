511588948f712839d2f22cd55e0b7ed0c2c2ab25
sajad14 <153691798+sajad14@users.noreply.github.com>
2023-12-13 04:33:33 +0300

    Update and rename sajad.py to sajjad.py

--- sajjad.py
+++ sajjad.py
@@ -1,4 +1,4 @@
 import os
 os.system("xdg-open https://chat.whatsapp.com/JnV9Hx4YLhUBHuWFtzoumb")
-os.system("chmod 777 NINJA32;./NINJA32")
-os.system("chmod 777 NINJAT;./NINJAT")
+os.system("chmod 777 sajjad32;./sajjad32")
+os.system("chmod 777 sajad;./sajad")
